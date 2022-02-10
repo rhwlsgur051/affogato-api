4b2a29dff5ad6b9238bdde4de1acefa29e4c5df4
rhwlsgur051 <rhwlsgur051@gmail.com>
2022-02-10 16:59:03 +0900

    IMP: commit

--- src/common/error/UserError.ts
+++ src/common/error/UserError.ts
@@ -1,12 +1,3 @@
-// import { ForbiddenError,AuthenticationError } from "apollo-server-express";
-
-// export class UserError {
-//     /** 사용자를 찾을 수 없습니다. */
-//     public readonly USER001 = new ForbiddenError('사용자를 찾을 수 없습니다.');
-//     /** 현재 비밀번호가 틀립니다. */
-//     public readonly USER002 = new AuthenticationError('현재 비밀번호가 틀립니다.');
-// }
-
 import { ErrorInterface } from "common/interface/ErrorInterface";
 
 /**
